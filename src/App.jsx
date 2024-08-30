b9e3f4142b04d1eb7ba8ef7e26fda254b8980811
sameersambhare <sameersambhare@gmail.com>
2024-08-31 02:22:07 +0530

    Adding the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg-black text-white'>
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/constants/data.js
@@ -0,0 +1,22 @@
+export const navMenu = [
+  {
+    id: "work",
+    url: "/work",
+  },
+  {
+    id: "about",
+    url: "/about",
+  },
+  {
+    id: "careers",
+    url: "/careers",
+  },
+  {
+    id: "contact",
+    url: "/contact",
+  },
+  {
+    id: "shop",
+    url: "/shop",
+  },
+];
